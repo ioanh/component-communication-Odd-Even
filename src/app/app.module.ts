25dea1440afc48dbd13211ce63fc7e58f4da575d
ioanh <ioan.xristov@abv.bg>
2021-04-07 17:49:58 +0300

    creating components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { OddComponent } from './odd/odd.component';
+import { EvenComponent } from './even/even.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OddComponent,
+    EvenComponent
   ],
   imports: [
     BrowserModule
