0dbf50c97ed8f66c3e7a3f3e88268e543a78716c
ioanh <ioan.xristov@abv.bg>
2021-04-07 18:48:29 +0300

    updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { OddComponent } from './odd/odd.component';
 import { EvenComponent } from './even/even.component';
+import { GameControlComponent } from './game-control/game-control.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     OddComponent,
-    EvenComponent
+    EvenComponent,
+    GameControlComponent
   ],
   imports: [
     BrowserModule
